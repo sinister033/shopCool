9fc2aed288f5adb85074ea27b4a715dda6d81b58
sinister033 <108584758+sinister033@users.noreply.github.com>
2023-01-03 19:04:23 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const adminController = require("../controllers/admin");
 const isAuth = require("../middleware/is-auth");
-const { body } = require("express-validator/check");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
